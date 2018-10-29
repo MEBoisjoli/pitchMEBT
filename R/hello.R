996a6db6959f9e24d62f4a672535ed4d9d3a4d27
MEBoisjoli <32574193+MEBoisjoli@users.noreply.github.com>
2018-10-28 21:26:14 -0400

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -14,7 +14,7 @@
 #' @param line_size Numeric. Default = 1
 #' @return A plot of a pitch
 #' @export
-create_Pitch <- function(grass_colour = "#2D2D2D", line_colour = "#EFEFEF", background_colour = "#2D2D2D", goal_colour = "#FFFFFF", goaltype = "barcanumbers", middlethird = FALSE, BasicFeatures = FALSE, JdeP = FALSE, arcs = TRUE, padding = 1.5, line_size = 1){
+create_Pitch <- function(grass_colour = "#274575", line_colour = "#EFEFEF", background_colour = "#274575", goal_colour = "#FFFFFF", goaltype = "barcanumbers", middlethird = FALSE, BasicFeatures = FALSE, JdeP = FALSE, arcs = TRUE, padding = 1.5, line_size = 1){
 
   library(ggplot2)
   ## set theme for blank pitch
